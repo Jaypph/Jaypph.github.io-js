b27d841332704955ec8af95a05e97b100181ffc5
Jaypph <linu1114@daksnet.dk>
2026-04-20 09:03:54 +0200

    Add initial chatbot responses to index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+module.exports = [
+    { id: 1, text: "hej hvordan kan jeg hjælpe dig", answer: "Hej! Hvordan kan jeg hjælpe dig?" },
+    { id: 2, text: "hvad koster produkter pris levering", answer: "Alt koster 20 kr og levering er 1-3 dage." },
+    { id: 3, text: "iphone apple cover modeller", answer: "Vi har covers til flere iPhone modeller." },
+    { id: 4, text: "levering fragt tid", answer: "Levering tager typisk 1-3 hverdage." },
+    { id: 5, text: "tak mange tak", answer: "Selv tak!" }
+];
